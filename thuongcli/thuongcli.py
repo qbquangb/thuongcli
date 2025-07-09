45938aadc6835bce0bcff24dcbf57ecd02fd4be6
qbquangbinh <qbqbqbqbqb123456789@gmail.com>
2025-07-09 21:29:45 +0700

    dgsdgg fhfh

--- thuongcli/thuongcli.py
+++ thuongcli/thuongcli.py
@@ -6,6 +6,7 @@ from thuonglib.delete_folder import clean_files_temp, del_dir_downloads
 from thuonglib.password_cipher import p_cipher
 from thuonglib.divide_merge_file import divide_file, merge_file
 from thuonglib.AES_CBC import encrypt_file_AES_CBC, decrypt_file_AES_CBC
+from thuonglib.RSA_OAEP import export_keys_RSA_OAEP, encrypt_file, decrypt_file
 
 def main():
     parser = argparse.ArgumentParser(
@@ -24,12 +25,20 @@ def main():
     parser.add_argument("--clean", "-c", action="store_true", help="Clean temporary files")
     parser.add_argument("--deepclean", "-dc", action="store_true", help="Xoa tat ca các tep va thu muc trong thu muc Downloads")
     parser.add_argument("--control_hand", "-ch", action="store_true", help="Dieu khien may tinh bang cu chi tay.")
-    parser.add_argument("--cipher", "-ci", action="store_true", help="Ma hoa van ban")
-    parser.add_argument("--encrypt_file", "-ef", action="store_true", help="Ma hoa file.")
-    parser.add_argument("--decrypt_file", "-df", action="store_true", help="Giai ma file.")
+    parser.add_argument("--cipher", "-ci", action="store_true", help="Ma hoa van ban bang XOR cipher.")
+    parser.add_argument("--encrypt_file", "-ef", action="store_true", help="Ma hoa file bằng XOR cipher.")
+    parser.add_argument("--decrypt_file", "-df", action="store_true", help="Giai ma file bằng XOR cipher.")
     parser.add_argument("--div_mer_file", "-dmf", action="store_true", help="Chia va ghep file.")
-    parser.add_argument("--encrypt_file_AES_CBC", "-efac", action="store_true", help="Ma hoa file.")
-    parser.add_argument("--decrypt_file_AES_CBC", "-dfac", action="store_true", help="Giai ma file.")
+    parser.add_argument("--encrypt_file_AES_CBC", "-efac", action="store_true", help="Ma hoa file bằng AES-CBC.")
+    parser.add_argument("--decrypt_file_AES_CBC", "-dfac", action="store_true", help="Giai ma file bằng AES-CBC.")
+    subparsers = parser.add_subparsers(dest='command', help="Chương trình tạo khóa RSA, mã hóa và giải mã file.")
+    subparsers.required = False
+
+    AES_RSA_parser = subparsers.add_parser("AES_RSA", help="Chương trình tạo khóa RSA, mã hóa và giải mã file bằng AES-CBC và RSA-OAEP.")
+    AES_RSA_parser.add_argument("--generate_keys", "-gk", action="store_true", help="Tạo cặp khóa RSA (public và private).")
+    AES_RSA_parser.add_argument("--encrypt_file", "-ef", action="store_true", help="Mã hóa file bằng AES-CBC và khóa RSA.")
+    AES_RSA_parser.add_argument("--decrypt_file", "-df", action="store_true", help="Giải mã file bằng AES-CBC và khóa RSA.")
+
     args = parser.parse_args()
     print(f"Hello, {args.name}!")
     if args.shutdown:
@@ -67,6 +76,15 @@ def main():
         encrypt_file_AES_CBC()
     elif args.decrypt_file_AES_CBC:
         decrypt_file_AES_CBC()
+    elif args.command == "AES_RSA":
+        if args.generate_keys:
+            export_keys_RSA_OAEP()
+        elif args.encrypt_file:
+            encrypt_file()
+        elif args.decrypt_file:
+            decrypt_file()
+    else:
+        parser.print_help()
 
 if __name__ == "__main__":
     main()
